710a2a9e5071203ffd57647e9be80eddccd51bc9
Augusto Scher <augusto.scher@resultadosdigitais.com.br>
2020-01-09 17:18:05 -0300

    Module 2 - Lists

--- 04-lists/for-forin/01-for-forin-forof.js
+++ 04-lists/for-forin/01-for-forin-forof.js
@@ -5,12 +5,25 @@ async function main() {
     const result = await service.getPeople('a');
     names = [];
 
-    console.time('for')
-    for (let i = 0; i <= result.results.length -1; i++) {
-      names.push(result.results[i].name);
+    // console.time('for')
+    // for (let i = 0; i <= result.results.length -1; i++) {
+    //   names.push(result.results[i].name);
+    // }
+    // console.timeEnd('for')
+
+    // console.time('forIn')
+    // for (let i in result.results) {
+    //   names.push(result.results[i].name);
+    // }
+    // console.timeEnd('forIn')
+
+    //mais performático
+    console.time('forOf')
+    for (person of result.results) {
+      names.push(person.name);
     }
-    console.timeEnd('for')
-    
+    console.timeEnd('forOf')
+
     console.log(names);
   } catch(error) {
     console.error('internal error', error);
