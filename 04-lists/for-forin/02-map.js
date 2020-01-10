c006300b79335018bf86d7e56c8672f4bb4b6c0d
Augusto Scher <augusto.scher@resultadosdigitais.com.br>
2020-01-10 10:09:59 -0300

    Module 2 - Lists

---
+++ 04-lists/for-forin/02-map.js
@@ -0,0 +1,28 @@
+const service = require('./service');
+
+async function main() {
+  try {
+    const result = await service.getPeople('a');
+
+    // const names = [];
+    // console.time('forEach')
+    // result.results.forEach(element => {
+    //   names.push(element.name)
+    // });
+    // console.timeEnd('forEach')
+    // console.log('names: ', names)
+
+    //map é mais performático
+    console.time('map')
+    const names2 = result.results.map(item => {
+      return item.name
+    })
+    console.timeEnd('map')
+    
+    console.log('names: ', names2)
+  } catch(error) {
+    console.error('Deu ruim: ', error);
+  }
+}
+
+main();
