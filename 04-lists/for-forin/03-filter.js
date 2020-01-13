8da491aca45d67586f32172d0216641c54440880
Augusto Scher <augusto.scher@resultadosdigitais.com.br>
2020-01-13 00:36:29 -0300

    Module 2 - Lists

--- 04-lists/for-forin/03-filter.js
+++ 04-lists/for-forin/03-filter.js
@@ -1,16 +1,35 @@
 const { getPeople } = require('./service');
 
+//Criando um filter customizado
+Array.prototype.myGreatFilter = function (callback) {
+  const list = []
+  for (index in this) {
+    const item = this[index];
+    const result = callback(item, index, this);
+    if (!result) continue
+    list.push(item)
+  }
+  return list;
+}
+
+
 async function main() {
   try {
     const { results } = await getPeople('a');
 
-    const larsFamily = results.filter(item => {
-      //Retornar boolean para cada item: true será considerado, false será removido
-
-      //Não encontrou: retora -1
-      //Encontrou: retorna a posição do item no array
+    //usando o filter customizado
+    const larsFamily = results.myGreatFilter((item, index, list) => {
+      // console.log(`index: ${index}, list: ${list.length}`)
       return item.name.toLowerCase().indexOf(`lars`) !== -1
     });
+
+    // const larsFamily = results.filter(item => {
+    //   //Retornar boolean para cada item: true será considerado, false será removido
+
+    //   //Não encontrou: retora -1
+    //   //Encontrou: retorna a posição do item no array
+    //   return item.name.toLowerCase().indexOf(`lars`) !== -1
+    // });
 
     const names = larsFamily.map(item => item.name);
     
