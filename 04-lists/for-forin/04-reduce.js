e3560af1db74c3ec1f6083ec6f8df35c2b403233
Augusto Scher <augusto.scher@resultadosdigitais.com.br>
2020-01-14 09:37:50 -0300

    Module 2 - Lists

--- 04-lists/for-forin/04-reduce.js
+++ 04-lists/for-forin/04-reduce.js
@@ -1,7 +1,11 @@
 const { getPeople } = require('./service');
 
-Array.prototype.myReduce = function(callback) {
-
+Array.prototype.myReduce = function(callback, initialValue) {
+  let finalValue = typeof initialValue !== undefined ? initialValue : this[0];
+  for (let i = 0; i <= this.length; i++){
+    finalValue = callback(finalValue, this[i], this);
+  }
+  return finalValue;
 }
 
 async function main() {
@@ -11,7 +15,21 @@ async function main() {
     const sum = heights.reduce((prev, next) => {
       return prev + next;
     }, 0);
+
     console.log('sum: ', sum);
+    console.log();
+
+    const myList = [
+      ['Augusto', 'Scher'],
+      ['Node', 'Javascript']
+    ]
+    let total = myList.myReduce((ant, prox) => {
+      return ant.concat(prox)
+    }, [])
+
+    console.log('total: ', total);
+    total = total.join(', ');
+    console.log('total: ', total);
   } catch(error) {
     console.error('Deu ruim: ', error);
   }
