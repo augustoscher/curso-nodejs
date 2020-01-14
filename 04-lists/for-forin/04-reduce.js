984019d0f2950cde33926f1475994a329e56c67a
Augusto Scher <augusto.scher@resultadosdigitais.com.br>
2020-01-14 00:19:37 -0300

    Module 2 - Lists

--- 04-lists/for-forin/04-reduce.js
+++ 04-lists/for-forin/04-reduce.js
@@ -3,7 +3,11 @@ const { getPeople } = require('./service');
 async function main() {
   try {
     const { results } = await getPeople('a');
-
+    const heights = results.map(item => parseInt(item.height))
+    const sum = heights.reduce((prev, next) => {
+      return prev + next;
+    });
+    console.log('sum: ', sum);
   } catch(error) {
     console.error('Deu ruim: ', error);
   }
