c56f5ad97b3eceb0d31805cf3bd4ef19509437eb
Augusto Scher <augusto.scher@resultadosdigitais.com.br>
2020-01-21 23:05:33 -0300

    Module 3 - Tests

--- 05-tests/01-test/service.js
+++ 05-tests/01-test/service.js
@@ -3,11 +3,19 @@ const URL = 'https://swapi.co/api/people';
 
 const getPeople = async (name) => {
   try {
-    const url = `${URL}/search=${name}&format=json`;
+    const url = `${URL}/?search=${name}&format=json`;
     const result = await axios.get(url);
-    return result.data
+    return result.data.results.map(mapPerson);
   } catch(error) {
     console.error(`service error: `, error);
+  }
+}
+
+const mapPerson = (item) => {
+  return {
+    name: item.name,
+    weight: item.mass,
+    height: item.height,
   }
 }
 
--- 05-tests/01-test/tests.js
+++ 05-tests/01-test/tests.js
@@ -9,7 +9,7 @@ const { getPeople } = require('./service');
 
 describe('Star Wars Tests', () => {
   it('Buscar R2D2 com o formato correto', async () => {
-    const expected = [{ name: 'R2-D2', weight: '96' }];
+    const expected = [{ name: 'R2-D2', weight: '32', height: '96' }];
     const baseName = 'r2-d2';
     const result = await getPeople(baseName);
     assert.deepEqual(result, expected);
