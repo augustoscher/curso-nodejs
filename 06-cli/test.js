011706bd6747a2a389a2f7a66e890fbe53f41f06
Augusto Scher <augusto.scher@resultadosdigitais.com.br>
2020-01-22 22:25:54 -0300

    Module 4 - CLI tools with node

--- 06-cli/test.js
+++ 06-cli/test.js
@@ -6,11 +6,12 @@ const DEFAULT_ITEM = { name: 'Flash', power: 'Speed', id: 1 };
 
 describe('Suite de manipulação de herois', () => {
 
-
   it('Deve pesquisar heroi usando arquivos', async () => {
     const expected = DEFAULT_ITEM;
-    const result = await database.listar(expected.id);
-    ok(result, expected);
+
+    //usa destructuring para pegar somente o primeiro elemento
+    const [result] = await database.listar(expected.id);
+    deepEqual(result, expected);
   });
 
   // it('Deve cadastrar heroi usando arquivos', () => {
