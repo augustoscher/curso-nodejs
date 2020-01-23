a1190bfe22f291f359c610c2a35a68076ea02e57
Augusto Scher <augusto.scher@resultadosdigitais.com.br>
2020-01-22 22:07:17 -0300

    Module 4 - CLI tools with node

--- 06-cli/test.js
+++ 06-cli/test.js
@@ -1,1 +1,12 @@
-const { deepEqual } = require('assert');
+const { deepEqual, ok } = require('assert');
+
+const DEFAULT_ITEM = { name: 'Flash', power: 'Speed', id: 1 };
+
+describe('Suite de manipulação de herois', () => {
+
+  it('Deve cadastrar heroi usando arquivos', () => {
+    const expected = DEFAULT_ITEM;
+
+    ok(null, expected);
+  });
+});
