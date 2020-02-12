41ff46fcf093da2941f55534b856824505ea2a2c
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-02-12 00:39:06 -0300

    06 - cli tool

--- 06-cli/index.js
+++ 06-cli/index.js
@@ -7,33 +7,47 @@ const Hero = require('./Hero');
 //async function main(){}
 
 //node index.js -s -n Flash -p Speed
+//node index.js -l
+//node index.js -r -i 1581478623768
 const main = async() => {
   commander
       .version('v1')
       .option('-n, --name [value]', "Hero Name")
       .option('-p, --power [value]', "Hero Power")
+      .option('-i, --id [value]', "Hero Id")
 
       .option('-s, --save', "Save Hero")
       .option('-l, --list', "List all Heros")
+      .option('-r, --remove', "Remove Hero by id")
       .parse(process.argv);
   
   const hero = new Hero(commander);
 
   try {
     if (commander.save) {
-      const resultado = await database.save(hero);
-      if (!resultado) {
+      delete hero.id;
+      const result = await database.save(hero);
+      if (!result) {
         console.error('Error on saving new Hero');
         return;
       }
+      console.log('Hero sucessfuly saved!')
     }
 
     if (commander.list) {
-      const resultado = await database.list();
-      console.log(resultado);
+      const result = await database.list();
+      console.log(result);
       return;
     }
-    
+
+    if (commander.remove) {
+      const result = await database.remove(hero.id);
+      if (!result) {
+        console.error('Error while removing Hero!')
+        return;
+      }
+      console.log('Hero sucessfuly removed!') 
+    }
   } catch(e) {
     console.error('Deu ruim', e)
   }
