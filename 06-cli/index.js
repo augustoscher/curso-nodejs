4b9c4919539f9ecc4344ed8c9d144d72bd0b0f61
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-02-12 00:23:36 -0300

    06 - cli tool

--- 06-cli/index.js
+++ 06-cli/index.js
@@ -1,13 +1,32 @@
 const commander = require('commander');
 const database = require('./database');
-
+const Hero = require('./Hero');
 
 //node index.js --help
+//node index.js -V
 //async function main(){}
+
+//node index.js -s -n Flash -p Speed
 const main = async() => {
-    commander
-        .version('v1')
-        .parse(process.argv);
+  commander
+      .version('v1')
+      .option('-n, --name [value]', "Hero Name")
+      .option('-p, --power [value]', "Hero Power")
+
+      .option('-s, --save', "Save Hero")
+      .parse(process.argv);
+  
+  const hero = new Hero(commander);
+  console.log(hero);
+
+  try {
+    if (commander.save) {
+      const resultado = await database.save(hero)
+    }
+    
+  } catch(e) {
+    console.error('Deu ruim', e)
+  }
 }
 
 main();
