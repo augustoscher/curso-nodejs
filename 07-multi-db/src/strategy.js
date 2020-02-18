ce99b296e5dc243940c6383b43a406e31d8659a4
Augusto Scher <augusto.scher@resultadosdigitais.com.br>
2020-02-18 10:59:24 -0300

    Module 5 - Multi DB

--- 07-multi-db/src/strategy.js
+++ 07-multi-db/src/strategy.js
@@ -21,3 +21,25 @@ class ICrud {
     throw new NotImplementedException();
   }
 }
+
+class ContextStrategy extends ICrud {
+  constructor(strategy) {
+    this._database = strategy;
+  }
+
+  create(item) {
+    this._database.create(item);
+  }
+
+  read(query) {
+    this._database.read(query);
+  }
+
+  update(id, item) {
+    this._database.update(id, item);
+  }
+
+  delete(id) {
+    this._database.delete(id);
+  }
+}
