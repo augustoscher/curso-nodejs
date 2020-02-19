beb10f37fef5d80df54aa683c983a2bdf6e7a6f1
Augusto Scher <augusto.scher@resultadosdigitais.com.br>
2020-02-18 22:15:37 -0300

    Module 06 - Relational DB

--- 08-multi-db-postgres/src/test.js
+++ 08-multi-db-postgres/src/test.js
@@ -1,6 +1,6 @@
-const PostgreSQL = require('../src/db/strategies/postgreSQL');
-const MongoDB = require('../src/db/strategies/mongodb'); 
-const ContextStrategy = require('../src/db/strategies/base/contextStrategy');
+const PostgreSQL = require('./db/strategies/postgreSQL');
+const MongoDB = require('./db/strategies/mongodb'); 
+const ContextStrategy = require('./db/strategies/base/contextStrategy');
 
 const main = () => {
   const contextMongo = new ContextStrategy(new MongoDB());
