e0521c4919b6e6bd0769b7d47e38a399121dead1
Augusto Scher <augusto.scher@resultadosdigitais.com.br>
2020-02-20 12:10:24 -0300

    test

---
+++ 06-cli/random-emails.js
@@ -0,0 +1,27 @@
+const faker = require('faker');
+
+const main = () => {
+  const mails = [];
+  for(let i = 0; i <= 14; i++) {
+    mails.push(faker.internet.exampleEmail());
+  }
+  console.log(mails);
+  console.log()
+
+  for(let i = 0; i <= 10; i++) {
+    console.log(mails[Math.floor(Math.random() * mails.length)]);
+  }
+
+  switch (process.env.LEAD_STRATEGY) {
+    case 'multiple_leads':
+      console.log('multiple');
+      break;
+    case 'specific_random_leads':
+      console.log('specific_random_leads')
+      break;
+    default:
+      console.log('single')
+  }
+}
+
+main();
