dc18f350fc7564162d3a929810b47e20e6bb0cb1
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-03-09 20:05:16 -0300

    Modulo 6 - Relational DB

---
+++ 08-multi-db-postgres/src/test/test/postgresStrategy.test.js
@@ -0,0 +1,14 @@
+const assert = require("assert");
+const Postgres = require("../../db/strategies/postgreSQL");
+const Context = require("../../db/strategies/base/contextStrategy");
+
+const context = new Context(new Postgres());
+
+describe("Postgres Strategy", () => {
+  this.timeout(Infinity);
+
+  it('Postgres Connection', async () => {
+    const result = await context.isConnected()
+    assert.equal(result, true)
+  })
+});
