b68fee519269b6b13d6d6151a562a8dcac3966c9
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-03-09 19:33:19 -0300

    Modulo 6 - Relational DB

--- 08-multi-db-postgres/src/db/strategies/postgreSQL.js
+++ 08-multi-db-postgres/src/db/strategies/postgreSQL.js
@@ -1,8 +1,12 @@
 const ICrud = require('./base/iCrud');
+const Sequelize = require("sequelize");
 
 class PostgreSQL extends ICrud {
   constructor(){
     super()
+    this._driver = null;
+    this._heroes = null;
+    this._connect();
   }
 
   create(item) {
@@ -21,8 +25,46 @@ class PostgreSQL extends ICrud {
    
   }
 
-  isConnected() {
+  async isConnected() {
     
+  }
+
+  defineModel() {
+    this._heroes = driver.define(
+      "heroes",
+      {
+        id: {
+          type: Sequelize.INTEGER,
+          required: true,
+          primaryKey: true,
+          autoIncrement: true
+        },
+        name: {
+          type: Sequelize.STRING,
+          required: true
+        },
+        power: {
+          type: Sequelize.STRING,
+          required: true
+        }
+      },
+      {
+        tableName: "heroes",
+        freezeTableName: false,
+        timestamps: false
+      }
+    );
+  
+    await heroes.sync();
+  }
+
+  _connect() {
+    this._driver = new Sequelize("heroes", "augustoscher", "minhasenhasecreta", {
+      host: "localhost",
+      dialect: "postgres",
+      quoteIdentifiers: false,
+      operatorAliases: false
+    });
   }
 }
 
