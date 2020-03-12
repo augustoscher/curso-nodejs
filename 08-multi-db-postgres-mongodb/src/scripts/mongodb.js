568f08b46d01ef1e66e6a2ce4cc4e8727e8fab64
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-03-12 00:04:54 -0300

    Modulo 7 - NoSQL DB

--- 08-multi-db-postgres-mongodb/src/scripts/mongodb.js
+++ 08-multi-db-postgres-mongodb/src/scripts/mongodb.js
@@ -1,3 +1,29 @@
+
 docker exec -it mongodb \
   mongo -u admin -p senhaadmin --authenticationDatabase admin
+
+//mostrar databases
+show dbs
+
+//configurando o contexto para heros db
+use heroes
+
+//mostrar collections
+show collections
+
+//find
+db.heroes.find({}).limit(1000).sort({"_id": 1 })
+db.heroes.find({}, {"_id": 0}).limit(1000).sort({"_id": 1 })
+
+//insert
+db.heroes.insert({name: 'Flash', power: 'Speed', birthDate: new Date()})
+
+for (let i = 0; i<100000; i++){
+    db.heroes.insert({name: `Clone${i+1}`, power: 'Speed', birthDate: new Date()})
+}
+
+//update
+db.heroes.update({name: 'Clone100'}, {name: 'Batman', power: 'Money'})
+
+//deleete
 
