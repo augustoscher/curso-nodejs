ccce59e1bfc9c2858ed53b9d8fabd2a4aa4d6843
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-03-12 09:36:18 -0300

    Modulo 7 - NoSQL DB

--- 09-multi-db-postgres-mongodb/src/mongodbExample.js
+++ 09-multi-db-postgres-mongodb/src/mongodbExample.js
@@ -21,3 +21,36 @@ connection.once("open", () => {
 //3- Desconecting
 // const state = connection.readyState;
 // console.log('state: ', state);
+
+const heroeSchema = new Mongoose.Schema({
+	name: {
+		type: String,
+		required: true
+	},
+	power: {
+		type: String,
+		required: true
+	},
+	createdAt: {
+		type: Date,
+		default: new Date()
+	}
+});
+
+const model = Mongoose.model('heroe', heroeSchema);
+
+const main = async () => {
+	const res = await model.create({
+		name: 'Batman',
+		power: 'Money'
+	});
+	console.log('Cadastrar: ', res);
+	console.log();
+
+	console.log('--- List ---')
+	const list = await model.find();
+	console.log(list);
+
+}
+
+main();
