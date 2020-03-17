6cc62a718c0e540e85bf9bbf9a996e4e8200cfa0
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-03-17 01:02:35 -0300

    09 - WebServices

--- 12-apis/src/test/apiHeros.test.js
+++ 12-apis/src/test/apiHeros.test.js
@@ -1,20 +1,19 @@
-const assert = require('assert');
-const api = require('../api');
+const assert = require("assert");
+const api = require("../api");
 
-describe('Heroes API', function() {
+describe("Heroes API", function() {
   this.beforeAll(async () => {
     app = await api;
   });
 
-  it('', async()=>{
+  it("", async () => {
     const result = await app.inject({
-      method: 'GET',
-      url: '/heroes'
-    })
+      method: "GET",
+      url: "/heroes"
+    });
 
     const data = JSON.parse(result.payload);
     assert.deepEqual(result.statusCode, 200);
     assert.ok(Array.isArray(data));
   });
-
 });
