a05e52997ee58a5bb557ffc42d4ddd30d69e72b1
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-04-10 11:58:49 -0300

    Printing values

--- 04-lists/for-forin/01-for-forin-forof.js
+++ 04-lists/for-forin/01-for-forin-forof.js
@@ -3,28 +3,35 @@ const service = require('./service');
 async function main() {
   try {
     const result = await service.getPeople('a');
+    let names = [];
+
+    console.time('for')
+    for (let i = 0; i <= result.results.length -1; i++) {
+      names.push(result.results[i].name);
+    }
+    console.timeEnd('for')
+    console.log('for: ', names);
+    console.log();
+
     names = [];
-
-    // console.time('for')
-    // for (let i = 0; i <= result.results.length -1; i++) {
-    //   names.push(result.results[i].name);
-    // }
-    // console.timeEnd('for')
-
-    // console.time('forIn')
-    // for (let i in result.results) {
-    //   names.push(result.results[i].name);
-    // }
-    // console.timeEnd('forIn')
+    console.time('forIn')
+    for (let i in result.results) {
+      names.push(result.results[i].name);
+    }
+    console.timeEnd('forIn')
+    console.log('forIn: ', names);
+    console.log();
 
     //mais performático
+    names = [];
     console.time('forOf')
     for (person of result.results) {
       names.push(person.name);
     }
     console.timeEnd('forOf')
+    console.log('forOf: ', names);
+    console.log();
 
-    console.log(names);
   } catch(error) {
     console.error('internal error', error);
   }
